9e067b3b9849f21475d5ac40422f47495140a1a8
kselvin <selvinkeith@gmail.com>
2018-03-07 09:50:38 -0500

    Initial Commit

---
+++ New York Schools/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  New York Schools
+//
+//  Created by Keith Selvin on 3/7/18.
+//  Copyright © 2018 Keith Selvin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
